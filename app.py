2f273b3aec70ef9c7f574a4f57e734ced07d35f7
issydoratesic-web <issydora.tesic@gmail.com>
2026-07-07 22:06:58 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -42,28 +42,14 @@ def spremi_termin(ime, kontakt, datum, vrijeme, usluga):
 st.set_page_config(page_title="Adora Rezervacije", layout="centered")
 stranica = st.sidebar.radio("Navigacija", ["Rezerviraj Termin", "Admin Panel"])
 
-# Mape bez cijena u nazivu
+# Ovdje su cijene naznačene u izborniku za klijenta
 usluge_mapa = {
-    "Šminkanje": ["Šminkanje", "Terensko šminkanje"],
-    "Oblikovanje i korekcija obrva": ["Oblikovanje obrva pincetom", "Oblikovanje i bojanje obrva", "Brow lift", "Brow lift i bojanje"],
-    "Tretmani lica": ["Enzimski piling", "Blagi mehanički piling", "Parenje toplim ručnikom i masaža uz piling"],
-    "Frizure": ["Ravnanje kose", "Uvijanje kose", "Hollywood valovi", "Elegantni repovi", "Punđa"],
-    "Ostale usluge": ["Relax zona"],
-    "Little Luxe Spa tretman": ["Mini", "Classic", "VIP"]
-}
-
-# Cjenik za sve usluge
-cijene_detaljno = {
-    "Šminkanje": "40€", "Terensko šminkanje": "50€",
-    "Oblikovanje obrva pincetom": "8€", "Oblikovanje i bojanje obrva": "15€", "Brow lift": "30€", "Brow lift i bojanje": "35€",
-    "Enzimski piling": "25€", "Blagi mehanički piling": "20€", "Parenje toplim ručnikom i masaža uz piling": "35€",
-    "Punđa": "15€", "Relax zona": "25€",
-    "Mini": "50€", "Classic": "70€", "VIP": "100€",
-    # Posebne cijene za frizure
-    "Ravnanje kose": {"Kratka kosa": "10€", "Duga kosa": "20€"},
-    "Uvijanje kose": {"Kratka kosa": "20€", "Duga kosa": "30€"},
-    "Hollywood valovi": {"Kratka kosa": "25€", "Duga kosa": "35€"},
-    "Elegantni repovi": {"Kratka kosa": "15€", "Duga kosa": "25€"}
+    "Šminkanje": ["Šminkanje - 40€", "Terensko šminkanje - 50€"],
+    "Oblikovanje i korekcija obrva": ["Oblikovanje obrva pincetom - 8€", "Oblikovanje i bojanje obrva - 15€", "Brow lift - 30€", "Brow lift i bojanje - 35€"],
+    "Tretmani lica": ["Enzimski piling - 25€", "Blagi mehanički piling - 20€", "Parenje toplim ručnikom i masaža uz piling - 35€"],
+    "Frizure": ["Ravnanje kose", "Uvijanje kose", "Hollywood valovi", "Elegantni repovi", "Punđa - 15€"],
+    "Ostale usluge": ["Relax zona - 25€"],
+    "Little Luxe Spa tretman": ["Mini - 50€", "Classic - 70€", "VIP - 100€"]
 }
 
 if stranica == "Rezerviraj Termin":
@@ -75,17 +61,19 @@ if stranica == "Rezerviraj Termin":
     kat = st.selectbox("Odaberite kategoriju:", list(usluge_mapa.keys()))
     usluga = st.selectbox("Odaberite uslugu:", usluge_mapa[kat])
     
+    # Logika za frizure i točne cijene po dužini
     puna_info = usluga
-    
-    # Logika za frizure - dodaje cijenu tek nakon odabira dužine
     if kat == "Frizure" and usluga in ["Ravnanje kose", "Uvijanje kose", "Hollywood valovi", "Elegantni repovi"]:
         duzina = st.radio("Odaberite dužinu kose:", ["Kratka kosa", "Duga kosa"])
-        cijena = cijene_detaljno[usluga][duzina]
-        puna_info = f"{usluga} ({duzina} - {cijena})"
-    else:
-        # Za ostale usluge, dohvati fiksnu cijenu
-        cijena = cijene_detaljno.get(usluga, "")
-        puna_info = f"{usluga} - {cijena}"
+        # Definiramo točne cijene ovdje
+        cijene_frizura = {
+            "Ravnanje kose": {"Kratka kosa": "10€", "Duga kosa": "20€"},
+            "Uvijanje kose": {"Kratka kosa": "20€", "Duga kosa": "30€"},
+            "Hollywood valovi": {"Kratka kosa": "25€", "Duga kosa": "35€"},
+            "Elegantni repovi": {"Kratka kosa": "15€", "Duga kosa": "25€"}
+        }
+        odabrana_cijena = cijene_frizura[usluga][duzina]
+        puna_info = f"{usluga} ({duzina} - {odabrana_cijena})"
 
     datum = st.date_input("Datum:", min_value=datetime.today().date())
     vremena = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"]
@@ -102,7 +90,6 @@ if stranica == "Rezerviraj Termin":
             st.error("Molimo ispunite ime i kontakt.")
 
 elif stranica == "Admin Panel":
-    # ... (ostatak admin panela ostaje isti)
     st.title("🔐 Admin Pristup")
     lozinka = st.text_input("Lozinka:", type="password")
     if lozinka == st.secrets.get("ADMIN_PASSWORD"):
